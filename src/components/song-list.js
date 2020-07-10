0b04e0aaf35f57167768f0972655f30ba51f28fa
Carolin Jeba <r.carolinjeba@outlook.com>
2020-07-10 10:31:17 +0530

    to know how useEffect works on  render/re-render

--- src/components/song-list.js
+++ src/components/song-list.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SongForm from './song-form'
 
 const SongList = () => {
 
-    const [songs, setSongs] = useState([{ title: "Because He lives, I can face tomorrow.", id: 1 }, { title: "God will take care of you.", id: 2 }]);
+    const [songs, setSongs] = useState([{ title: "Because He lives, I can face tomorrow.", id: 1 }, { title: "God will take care of you.", id: 2 }]);    
+
+    const [ number, setNumber ] = useState(1); // to learn how multiple useState can be added
 
     const AddSongHandler = (title) => {
         setSongs([...songs, { title, id: Math.random().toString() }])  // Math.random().toString() generates random and unique key 
     }
+
+    /* region To learn how useEffect works on every render/re-render */
+
+    useEffect(() => {
+        console.log('Fired only when the dependency(songs) changes')
+    }, [songs]);
+
+    useEffect(() => {
+        console.log(number);
+        console.log('Fired only when the dependency(number) changes')
+    }, [number]);
+
+    /* end region*/
 
     return (
         <div className="song-list">
@@ -19,6 +34,8 @@ const SongList = () => {
                 })}
             </ul>
             <SongForm addSong = {AddSongHandler} />
+            
+            <button onClick= {() =>{ setNumber(number + 1)}}> Add Song Number </button> 
         </div>
     );
 }
