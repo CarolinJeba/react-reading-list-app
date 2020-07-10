9e84e4c55c8878aaa31d9e9c5edff7b316ee2cb9
Carolin Jeba <r.carolinjeba@outlook.com>
2020-07-10 10:01:33 +0530

    useState in Form + passing function as prop

---
+++ src/components/song-form.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+const SongForm = ({addSong}) => {
+
+    const [title, setTitle] = useState('');
+
+    const titleSubmitHandler = (event) => {
+        event.preventDefault();
+        addSong(title);
+        setTitle(''); //to clear the input field
+    }
+    return (
+        <form onSubmit={titleSubmitHandler}>
+            <label>Add Song form</label>
+            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}
+            />
+            <input type="submit" value = "Add Title to Song List" />
+        </form>
+    );
+}
+
+export default SongForm;
--- src/components/song-list.js
+++ src/components/song-list.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import SongForm from './song-form'
 
 const SongList = () => {
 
     const [songs, setSongs] = useState([{ title: "Because He lives, I can face tomorrow.", id: 1 }, { title: "God will take care of you.", id: 2 }]);
 
-    const onClickAddSong = () => {
-        setSongs([...songs, { title: "Amazing Grace", id: 3 }])
+    const AddSongHandler = (title) => {
+        setSongs([...songs, { title, id: Math.random().toString() }])  // Math.random().toString() generates random and unique key 
     }
 
     return (
@@ -17,9 +18,11 @@ const SongList = () => {
                     );
                 })}
             </ul>
-            <button className="add-song" onClick={onClickAddSong}> Add Song </button>
+            <SongForm addSong = {AddSongHandler} />
         </div>
     );
 }
 
 export default SongList;
+
+
